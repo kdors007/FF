e914c2fcfa04a3d9664d9a1d317c5b36d4e4db90
Jacquelyn Kominik <jkominik@gmail.com>
2017-10-22 17:43:39 -0400

    Finished adding name to post comments

--- icebreakerz2/app/controllers/comments_controller.rb
+++ icebreakerz2/app/controllers/comments_controller.rb
@@ -5,7 +5,7 @@ class CommentsController < ApplicationController
 def create
 	
 	@post = Post.find(params[:post_id])
-	@comment = @post.comments.create(params[:comment].permit(:name, :body))
+	@comment = @post.comments.create(params[:comment].permit(:name, :body, :user_id))
 	redirect_to post_path(@post)
 end
 
